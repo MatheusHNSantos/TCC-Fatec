5a2dd46ea8d1952479c98ae53f03f09dc63e801a
Luís Felipe Mantoan <luisfelipemantoan@gmail.com>
2018-02-16 00:07:06 -0200

    Corrige typo

--- src/main/java/model/dao/person/PersonDAO.java
+++ src/main/java/model/dao/person/PersonDAO.java
@@ -14,13 +14,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 
 /**
  *
- * @author felipemantoan
+ * @author Felipemantoan
  */
 public abstract class PersonDAO implements DAO {
 
